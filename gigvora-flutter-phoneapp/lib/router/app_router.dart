4472918181f8beb63a45bdf31ec3196579e3335e
orbas1 <ironcrest96@gmail.com>
2025-10-14 00:29:47 +0100

    Add enterprise-grade connection network flows across platforms

--- gigvora-flutter-phoneapp/lib/router/app_router.dart
+++ gigvora-flutter-phoneapp/lib/router/app_router.dart
@@ -21,6 +21,7 @@ import '../features/messaging/presentation/inbox_screen.dart';
 import '../features/services/presentation/service_operations_screen.dart';
 import '../features/mentorship/presentation/mentorship_screen.dart';
 import '../features/project_gig_management/presentation/project_gig_management_screen.dart';
+import '../features/connections/presentation/connections_screen.dart';
 
 final _rootNavigatorKey = GlobalKey<NavigatorState>();
 
@@ -44,6 +45,7 @@ final appRouterProvider = Provider<GoRouter>((ref) {
       GoRoute(path: '/volunteering', builder: (context, state) => const VolunteeringScreen()),
       GoRoute(path: '/notifications', builder: (context, state) => const NotificationsScreen()),
       GoRoute(path: '/inbox', builder: (context, state) => const InboxScreen()),
+      GoRoute(path: '/connections', builder: (context, state) => const ConnectionsScreen()),
       GoRoute(path: '/operations', builder: (context, state) => const ServiceOperationsScreen()),
       GoRoute(path: '/dashboard/mentor', builder: (context, state) => const MentorshipScreen()),
       GoRoute(
