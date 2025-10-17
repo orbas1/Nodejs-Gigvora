07297453fa6dcf2928335cdc32031dc95da3db5f
orbas1 <ironcrest96@gmail.com>
2025-10-17 23:45:47 +0100

    Rebuild agency ID verification module

---
+++ gigvora-frontend-reactjs/src/constants/agencyDashboardMenu.js
@@ -0,0 +1,26 @@
+export const AGENCY_DASHBOARD_MENU_SECTIONS = [
+  {
+    id: 'ops',
+    label: 'Ops',
+    items: [
+      {
+        id: 'agency-overview',
+        name: 'Home',
+        sectionId: 'agency-overview',
+      },
+    ],
+  },
+  {
+    id: 'risk',
+    label: 'Risk',
+    items: [
+      {
+        id: 'agency-id-verification',
+        name: 'IDs',
+        sectionId: 'agency-id-verification',
+      },
+    ],
+  },
+];
+
+export default AGENCY_DASHBOARD_MENU_SECTIONS;
